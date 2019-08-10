28cadbf9707a848a75ae97381b3b7af22bebf24a
phymhan <hanligong@gmail.com>
2019-08-10 14:03:13 -0400

    rm ckpt

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,9 +1,18 @@
-import os
+# import os
 
-src = '/media/ligong/Picasso/Share/cbimfs/Research/MURI/openface/clips-r3'
+# src = '/media/ligong/Picasso/Share/cbimfs/Research/MURI/openface/clips-r3'
+# names = os.listdir(src)
+
+# for name in names:
+#     img_path = os.path.join(src, name, f'{name}_aligned')
+#     vid_path = os.path.join(src, name, f'aligned.mp4')
+#     os.system(f'ffmpeg -r 30 -f image2 -i {img_path}/frame_det_00_%06d.bmp -vcodec libx264 -crf 25 -pix_fmt yuv420p {vid_path}')
+
+src = '/media/ligong/Picasso/Share/cbimfs/Research/MURI/clips/r3'
 names = os.listdir(src)
-
 for name in names:
-    img_path = os.path.join(src, name, f'{name}_aligned')
+    in_file = os.path.join(src, f'{name}')
+    name_ = name.split('_')
+    out_file = os.path.join(src, f'{name_[0]}-{}')
     vid_path = os.path.join(src, name, f'aligned.mp4')
     os.system(f'ffmpeg -r 30 -f image2 -i {img_path}/frame_det_00_%06d.bmp -vcodec libx264 -crf 25 -pix_fmt yuv420p {vid_path}')
