2f48e5987328e4603dc15de1e035a96c065e5473
phymhan <hanligong@gmail.com>
2019-08-29 14:46:27 -0400

    run

--- bash_scripts/hp-norm.sh
+++ bash_scripts/hp-norm.sh
@@ -4,32 +4,35 @@ GPUID=5
 NAME=norm
 
 # videov2
-for VAR in norm none
+for VAR in 'norm --norm_fc norm' 'none --norm_fc norm' 'none --norm_fc none' 'norm --norm_fc none'
 do
 	CUDA_VISIBLE_DEVICES=${GPUID} python main.py \
     --name hp-${NAME}_video_${VAR} \
     --print_freq 1 \
     --setting videov2 \
-    --norm ${VAR}
+    --dim_fc 64 \
+    --norm_input_map ${VAR}
 done
 
 # weakly
-for VAR in norm none
+for VAR in 'norm --norm_fc norm' 'none --norm_fc norm' 'none --norm_fc none' 'norm --norm_fc none'
 do
 	CUDA_VISIBLE_DEVICES=${GPUID} python main.py \
     --name hp-${NAME}_weakly_${VAR} \
     --print_freq 1 \
     --setting weakly \
-    --norm ${VAR}
+    --dim_fc 64 \
+    --norm_input_map ${VAR}
 done
 
 # weakly+noisy
-for VAR in norm none
+for VAR in 'norm --norm_fc norm' 'none --norm_fc norm' 'none --norm_fc none' 'norm --norm_fc none'
 do
 	CUDA_VISIBLE_DEVICES=${GPUID} python main.py \
     --name hp-${NAME}_weakly+noisy_${VAR} \
     --print_freq 1 \
     --setting weakly \
     --noisy true \
-    --norm ${VAR}
+    --dim_fc 64 \
+    --norm_input_map ${VAR}
 done
